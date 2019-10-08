42d88eaa7cdb99bd6fca7ac742b211da645cd316
krikaz <pascal.bou@protonmail.com>
2019-10-08 13:41:26 +0200

    corrected merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,16 @@
+import math
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    merged_arr = []
     # TO-DO
     while arrA and arrB:
-        if arrA[0] > arrB[0]:
+        if arrA[0] < arrB[0]:
+            merged_arr.append(arrA[0])
+            arrA.remove(arrA[0])            
+        else:
             merged_arr.append(arrB[0])
             arrB.remove(arrB[0])
-        else:
-            merged_arr.append(arrA[0])
-            arrA.remove(arrA[0])
 
     while arrA:
         merged_arr.append(arrA[0])
@@ -25,19 +26,30 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    n = len(arr) - 1
-    if n == 1:
+    l = len(arr)
+    if l <= 1:
         return arr
 
-    arrayOne = [n for n in arr[:int(n/2)]]
-    arrayTwo = [n for n in arr[int(n/2+1):]]
+    arrayOne = [n for n in arr[:math.floor(l/2)]]
+    arrayTwo = [n for n in arr[math.floor(l/2):]]
+
+    # print(arrayOne)
+    # print(arrayTwo)
 
     arrayOne = merge_sort(arrayOne)
     arrayTwo = merge_sort(arrayTwo)
 
+    # print(arrayOne)
+    # print(arrayTwo)
+
     arr = merge(arrayOne, arrayTwo)
+    # print(arr)
 
     return arr
+
+# a = [4,9,3,2,7,5,8,1,6,0]
+# x = merge_sort(a)
+# print(x)
 
 
 # STRETCH: implement an in-place merge sort algorithm
